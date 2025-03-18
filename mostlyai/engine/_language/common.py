cf47c71a3ffe53d874dd2f818d49df9a24f9cab9
Radu Rogojanu <radu.rogojanu@mostly.ai>
2025-03-18 10:10:26 +0100

    fix: update warnings for missing bitsandbytes and vllm on CUDA during training and generation

--- mostlyai/engine/_language/common.py
+++ mostlyai/engine/_language/common.py
@@ -75,10 +75,11 @@ def load_base_model_and_config(
 
     # Load pretrained base model
     use_cache = not is_training  # KV cache is not needed during training
-    is_gpu_training = is_training and device.type == "cuda" and importlib.util.find_spec("bitsandbytes") is not None
-    if is_training and device.type == "cuda" and importlib.util.find_spec("bitsandbytes") is None:
+    is_gpu_training = is_training and device.type == "cuda"
+    is_bitsandbytes_available = importlib.util.find_spec("bitsandbytes") is not None
+    if is_gpu_training and not is_bitsandbytes_available:
         _LOG.warning(
-            "CUDA device was found but bitsandbytes is not available. Please use `pip install -U 'mostlyai-engine[gpu]'` to install bitsandbytes for quantization."
+            "CUDA device was found but bitsandbytes is not available. Please use extra [gpu] to install bitsandbytes for quantization."
         )
     bf16_supported = is_bf16_supported(device)
     if bf16_supported:
@@ -87,7 +88,7 @@ def load_base_model_and_config(
     else:
         attn_implementation = None
         torch_dtype = torch.float32
-    if is_gpu_training:
+    if is_gpu_training and is_bitsandbytes_available:
         quantization_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
