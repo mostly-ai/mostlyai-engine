183c669a593dcda4f77d7f8a1b8923b10c83e7a3
Lukasz Kolodziejczyk <lukasz.kolodziejczyk@mostly.ai>
2025-07-14 12:56:05 +0200

    wip

--- mostlyai/engine/_tabular/generation.py
+++ mostlyai/engine/_tabular/generation.py
@@ -260,17 +260,17 @@ def _continue_sequence_mask(
     syn[SLEN_SUB_COLUMN_PREFIX] = decode_slen_sidx_sdec(syn, seq_len_max, prefix=SLEN_SUB_COLUMN_PREFIX)
     syn[STOP_SUB_COLUMN_PREFIX] = decode_slen_sidx_sdec(syn, seq_len_max, prefix=STOP_SUB_COLUMN_PREFIX)
     syn[SLEN_SUB_COLUMN_PREFIX] = np.maximum(seq_len_min, syn[SLEN_SUB_COLUMN_PREFIX])
-    # if prev_slen is not None:
-    #     prev_slen_mask = syn[SIDX_SUB_COLUMN_PREFIX] >= prev_slen
-    #     syn.loc[prev_slen_mask, SLEN_SUB_COLUMN_PREFIX] = prev_slen[prev_slen_mask].values
+    if prev_slen is not None:
+        prev_slen_mask = syn[SIDX_SUB_COLUMN_PREFIX] >= prev_slen
+        syn.loc[prev_slen_mask, SLEN_SUB_COLUMN_PREFIX] = prev_slen[prev_slen_mask].values
     # calculate stop sequence mask (True=continue, False=stop)
     print(syn["tgt:/__sidx_"].value_counts())
     print(syn["tgt:/__slen_"].value_counts())
     print(syn["tgt:/__stop_"].value_counts())
-    continue_mask = syn[STOP_SUB_COLUMN_PREFIX] == 1
-    return continue_mask
-    # continue_mask = syn[SIDX_SUB_COLUMN_PREFIX] < syn[SLEN_SUB_COLUMN_PREFIX]
-    # return continue_mask , syn[SLEN_SUB_COLUMN_PREFIX][continue_mask].reset_index(drop=True)
+    # continue_mask = syn[STOP_SUB_COLUMN_PREFIX] == 1
+    # return continue_mask
+    continue_mask = syn[SIDX_SUB_COLUMN_PREFIX] < syn[SLEN_SUB_COLUMN_PREFIX]
+    return continue_mask, syn[SLEN_SUB_COLUMN_PREFIX][continue_mask].reset_index(drop=True)
 
 
 def _post_process_decoding(
@@ -965,7 +965,7 @@ def generate(
                 step_ctx_keys = ctx_keys
                 step_size = batch_size
                 # step_size_drop_threshold = max(50, batch_size // 100)
-                # prev_slen = None
+                prev_slen = None
                 for seq_step in range(seq_steps):
                     print(f"SEQ_STEP: {seq_step}")
                     # exit early if nothing more to sample
@@ -1004,14 +1004,14 @@ def generate(
                     out_pt = torch.stack(list(out_dct.values()), dim=0).transpose(0, 1)
                     # calculate continue sequence mask
                     # continue_mask, prev_slen = _continue_sequence_mask(
-                    continue_mask = _continue_sequence_mask(
+                    continue_mask, prev_slen = _continue_sequence_mask(
                         step_output=out_pt,
                         sub_cols=tgt_sub_columns,
                         step_keys=step_ctx_keys,
                         key_name=tgt_context_key,
                         seq_len_min=seq_len_min,
                         seq_len_max=seq_len_max,
-                        # prev_slen=prev_slen,
+                        prev_slen=prev_slen,
                     )
                     next_step_size = continue_mask.sum()
                     # filter next iteration inputs only when threshold is passed
