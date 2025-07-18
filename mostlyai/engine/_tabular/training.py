ade41ebd206213ba9e42b8e3e24d5e99c290e02b
Lukasz Kolodziejczyk <lukasz.kolodziejczyk@mostly.ai>
2025-07-18 11:19:13 +0200

    complete

--- mostlyai/engine/_tabular/training.py
+++ mostlyai/engine/_tabular/training.py
@@ -288,7 +288,7 @@ def _calculate_sample_losses(
                 mask = slen_mask
 
             column_loss = criterion(output[col].transpose(1, 2), data[col].squeeze(2))
-            masked_loss = torch.sum(column_loss * mask, dim=1) / torch.clamp(torch.sum(mask >= 1, dim=1), min=1)
+            masked_loss = torch.sum(column_loss * mask, dim=1)
             losses_by_column.append(masked_loss)
     else:
         losses_by_column = [criterion(output[col], data[col].squeeze(1)) for col in tgt_cols]
