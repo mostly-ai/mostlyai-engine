4a90c07915f5c40c5322a31d0d3051574cd57f1c
michdr <michael.druk@mostly.ai>
2025-11-17 11:26:43 +0100

    fix: do not set_random_state for the same pid

--- mostlyai/engine/_tabular/encoding.py
+++ mostlyai/engine/_tabular/encoding.py
@@ -14,6 +14,7 @@
 
 import logging
 import time
+from multiprocessing import current_process
 from pathlib import Path
 
 import numpy as np
@@ -233,6 +234,7 @@ def encode_df(
                 values=df[column],
                 column_stats=column_stats,
                 context_keys=context_keys,
+                parent_pid=current_process().pid,
             )
         )
     if delayed_encodes:
@@ -247,8 +249,10 @@ def _encode_col(
     values: pd.Series,
     column_stats: dict,
     context_keys: pd.Series | None = None,
+    parent_pid: int | None = None,
 ) -> pd.DataFrame:
-    set_random_state(worker=True)
+    if current_process().pid != parent_pid:
+        set_random_state(worker=True)
     is_sequential_column = is_sequential(values)
     if is_sequential_column:
         # explode nested columns and encode the same way as flat columns
