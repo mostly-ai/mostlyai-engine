bcfa1102970f35ddb2828692f503f61454914df4
Michael Platzer <michael.platzer@gmail.com>
2025-12-13 11:35:56 +0100

    ref

--- tests/end_to_end/test_tabular_flat.py
+++ tests/end_to_end/test_tabular_flat.py
@@ -585,9 +585,9 @@ class TestTabularFlatWithContext:
         assert abs((syn_time.quantile(0.8) - orig_time.quantile(0.8)).days) < 365 * 3
 
         # check consistency between date and datetime
-        assert (syn_ctx["date"].dt.year == syn_tgt["datetime"].dt.year).mean() > 0.9
-        assert (syn_ctx["date"].dt.month == syn_tgt["datetime"].dt.month).mean() > 0.9
-        assert (syn_ctx["date"].dt.day == syn_tgt["datetime"].dt.day).mean() > 0.9
+        assert (syn_ctx["date"].dt.year == syn_tgt["datetime"].dt.year).mean() > 0.8
+        assert (syn_ctx["date"].dt.month == syn_tgt["datetime"].dt.month).mean() > 0.8
+        assert (syn_ctx["date"].dt.day == syn_tgt["datetime"].dt.day).mean() > 0.8
 
         orig_latlong = split_str_to_latlong(orig_tgt["geo"])
         orig_latlong_mean = orig_latlong.dropna().astype(float).mean()
